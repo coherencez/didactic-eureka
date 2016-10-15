5e4f8980ade79b580a75ce94cac42b42e26c3c49
Thomas Swann <tkswann@mac.com>
2016-10-14 19:44:29 -0500

    basic stream listening for hashtags

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const Twitter = require('twitter')
 
 const client = new Twitter(apiKeys)
 
-client.post('statuses/update', {status: 'Testing TwitterAPI connection'}, (err, twt, res) => {
-  if(err) {
-    console.error(err)
-  }
-  console.log(twt)
-  console.log(res)
+// client.post('statuses/update', {status: 'Testing TwitterAPI connection'}, (err, twt, res) => {
+//   if(err) {
+//     console.error(err)
+//   }
+//   console.log(twt)
+//   console.log(res)
+// })
+
+const stream = client.stream('statuses/filter', {track: '#mentor'})
+stream.on('data', (event) => {
+  console.log('EVENT:', event && event.text)
+})
+stream.on('error', (error) => {
+  console.error(error)
 })
