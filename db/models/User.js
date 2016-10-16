945479d113756b798244e7262a2b50609667d921
Thomas Swann <tkswann@mac.com>
2016-10-16 17:30:12 -0500

    bcrypt added but not being used for now -- overkill at the moment

--- db/models/User.js
+++ db/models/User.js
@@ -1,8 +1,9 @@
 'use strict'
 
-const mongoose = require('mongoose')
+const          mongoose = require('mongoose')
+// const { hash, compare } = require('bcrypt')
 
-const BCRYPT_DIFFICULTY = 15
+// const BCRYPT_DIFFICULTY = 15
 const HTML5_EMAIL_VALIDATION = /^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
 const userSchema = mongoose.Schema({
@@ -14,5 +15,23 @@ const userSchema = mongoose.Schema({
 	},
 	phone: {type: String, required: true},
 })
+
+// userSchema.pre('save', function(next) {
+// 	const user = this
+// 	hash(user.phone, BCRYPT_DIFFICULTY, (err, hashedPhone) => {
+// 		if(err) { return next(err) }
+// 		user.phone = hashedPhone
+//     next()
+// 	})
+// })
+//
+// userSchema.pre('save', function(next) {
+//   const user = this
+//   hash(user.email, BCRYPT_DIFFICULTY, (err, hashedEmail) => {
+//     if(err) { return next(err) }
+//     user.email = hashedEmail
+//     next()
+//   })
+// })
 
 module.exports = mongoose.model('User', userSchema)
