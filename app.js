170d19da2f835ce18205162918bfb559921142c0
Thomas Swann <tkswann@mac.com>
2016-10-16 01:08:51 -0500

    twilio added for text message alerts

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict'
-const apiKeys = require('./secretKey')
-const Twitter = require('twitter')
+const {twitterCreds, twilioCreds:{accountSid,authToken}} = require('./secretKey')
+const twitterClient = require('twitter')
+const Twilio = require('twilio')(accountSid,authToken)
+const fs = require('fs')
 
-const client = new Twitter(apiKeys)
+const Twitter = new twitterClient(twitterCreds)
 
-// client.post('statuses/update', {status: 'Testing TwitterAPI connection'}, (err, twt, res) => {
+// Twitter.post('statuses/update', {status: 'Testing TwitterAPI connection'}, (err, twt, res) => {
 //   if(err) {
 //     console.error(err)
 //   }
@@ -12,10 +14,29 @@ const client = new Twitter(apiKeys)
 //   console.log(res)
 // })
 
-const stream = client.stream('statuses/filter', {track: '#mentor'})
-stream.on('data', (event) => {
-  console.log('EVENT:', event && event.text)
-})
-stream.on('error', (error) => {
-  console.error(error)
+Twitter.stream('statuses/filter', {track: 'javascript,mentor,web developer'}, stream => {
+  stream.on('data', ({user, text}) => {
+    console.log(text)
+    Twilio.messages.create({
+      to: "+16158123396",
+      from: "+16158618139",
+      body: `@${user.screen_name}: ${text}`,
+      // mediaUrl: "https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
+    }, function(err, message) {
+        if(err) throw err
+        console.log(message.sid);
+    });
+
+
+      // Twitter.post('statuses/update', {status: tweet.text}, (err, twt, res) => {
+      //   if(err){
+      //     console.error(err)
+      //   }
+      //   console.log(twt)
+      //   console.log(res)
+      // })
+  })
+  stream.on('error', error => {
+    console.error(error)
+  })
 })
