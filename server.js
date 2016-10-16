cfca928af477a0bb6c87555fd4bcab7904e248af
Thomas Swann <tkswann@mac.com>
2016-10-16 16:40:08 -0500

    added body-parser to form information

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 'use strict'
 const express = require('express')
+  ,bodyParser = require('body-parser')
   ,      PORT = process.env.PORT || 3000
   ,       app = express()
 
 app.set('port', PORT)
 app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static('public'))
 
 
 app.get('/', (req,res) => {
   res.render('index')
+})
+
+app.post('/', ({body: {email, phone}},res) => {
+  console.log(email, phone)
 })
 
 app.listen(PORT, () => {
